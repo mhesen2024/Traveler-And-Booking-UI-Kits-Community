c2af4e0ae0bbc07609e83d179d850f6fc1f68ea0
TamaraElyyan <tamaraelyyan1@gmail.com>
2024-08-04 01:18:38 +0300

    Add files via upload

---
+++ src/views/Home.jsx
@@ -0,0 +1,11 @@
+import Header from '../components/organisem/Header'
+import OffersLink from '../components/organisem/offersLink'
+
+export default function Home() {
+  return (
+    <div className=''>
+      <Header/>
+      <OffersLink/>
+    </div>
+  )
+}
